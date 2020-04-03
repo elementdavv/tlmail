bbe72c88aec7a78002fb88c6823fbf63c813a666
lakedai <vinctai@gmail.com>
2020-04-03 14:54:14 +0800

    add dkim support

--- tlmail.py
+++ tlmail.py
@@ -6,6 +6,7 @@ import time
 from datetime import datetime
 import smtplib
 import dns.resolver
+import dkim
 from email.header import decode_header, make_header
 from aiosmtpd.controller import Controller
 
@@ -30,6 +31,7 @@ ADDRESSGAP 	= '###{}###'
 SUBJECTTAG 	= b'=\?.+\?[b|B]\?'
 EMAILWRAP 	= '<(.+)>'
 
+#logging.basicConfig(level=logging.DEBUG)
 log = logging.getLogger(__name__)
 log.setLevel(logging.DEBUG)
 logfh = logging.FileHandler(__PROJECTNAME_ + '.log')
@@ -38,11 +40,14 @@ logfh.setFormatter(logfmt)
 log.addHandler(logfh)
 
 class MailProxyHandler:
-    def __init__(self, domain, host, user, port, relay_ip, relay_port):
+    def __init__(self, domain, host, user, use_dkim, port, dkim_selector, dkim_key, relay_ip, relay_port):
         self._domain = domain
         self._host = host
         self._user = user
+        self._use_dkim = use_dkim,
         self._port = port
+        self._dkim_selector = dkim_selector
+        self._dkim_key = dkim_key
         self._relay_ip = relay_ip
         self._relay_port = relay_port
 
@@ -79,21 +84,17 @@ class MailProxyHandler:
                 return '250 OK'
 
         else:
-            # relay just redirect mail intact
-            # e = self.rtoe(envelope.rcpt_tos[0])
+            # relay just redirect intact
             self._mx = self.exchanger(envelope.rcpt_tos[0].split('@')[1])
             if not self._mx:
                 log.warning('bad outbound address: ' + envelope.rcpt_tos[0])
                 return '250 OK'
                 
-        refused = self._deliver(envelope)
-        if refused:
-            log.warning('recipients refused: %s', refused)
+        self._deliver(envelope)
 
         return '250 OK'
 
     def _deliver(self, envelope):
-        refused = {}
         try:
             s = smtplib.SMTP()
             if __SERVER_MODE__:
@@ -118,15 +119,15 @@ class MailProxyHandler:
                     )
                 finally:
                     s.quit()
-        except smtplib.SMTPRecipientsRefused as e:
-            refused = e.recipients
 
-        except (OSError, smtplib.SMTPException) as e:
-            refused = envelope.rcpt_tos
+        except (smtplib.SMTPConnectError,
+                smtplib.SMTPRecipientsRefused,
+                smtplib.SMTPSenderRefused,
+                smtplib.SMTPDataError, 
+                smtplib.SMTPException) as e:
+            log.error(e.message)
 
-        return refused
-    
-    # from mail line get mail address
+    # from/to mail line get mail address
     def rtoe(self, r):
         e = re.findall(EMAILWRAP, r)
         return e[0] if e else r
@@ -137,8 +138,12 @@ class MailProxyHandler:
         return str(mx[0].exchange).rstrip('.') if mx else None
     
     # generate dkim
-    def dkim(self):
-        return ''
+    def dkim(self, content):
+        return dkim.sign(message = content, 
+                         selector = self._dkim_selector.encode(), 
+                         domain = self._domain.encode(), 
+                         privkey = self._dkim_key.encode(),
+                         include_headers = ['From', 'To', 'Subject'])
         
     # genereate messageid
     def messageid(self):
@@ -189,9 +194,22 @@ class MailProxyHandler:
                         v['no'] = i
                         break
             i += 1
-
+            
+        # if subject is multiline
+        sts = (ST1, ST2)
+        si = marks['subject']['no']
+        subjectline = lines[si]
+        cont = True
+        while cont:
+            cont = False
+            si += 1
+            for st in sts:
+                if lines[si].startswith(st):
+                    subjectline += lines[si]
+                    cont = True
+                    break
+        
         # construct envelope subject/from/tos
-        subjectline = lines[marks['subject']['no']]
         subjectenc = re.search(SUBJECTTAG, subjectline)
         if subjectenc:
             subj = subjectline.lstrip(SUBJECT).rstrip(CRLF)
@@ -200,15 +218,15 @@ class MailProxyHandler:
             
         if self._inbound is True:
             if subjectenc:
-                ds1 = ds1 + ADDRESSGAP.format(envelope.mail_from).encode()
+                ds1 = ds1 + ADDRESSGAP.format(self.rtoe(envelope.mail_from)).encode()
                 subj = make_header([(ds1, ds[0][1])]).encode().encode()
                 linesubject = SUBJECT + subj + CRLF
             else:
-                linesubject = subjectline.replace(CRLF, ADDRESSGAP.format(envelope.mail_from).encode() + CRLF)
-            envelope.mail_from = envelope.rcpt_tos[0]
+                linesubject = subjectline.replace(CRLF, ADDRESSGAP.format(self.rtoe(envelope.mail_from)).encode() + CRLF)
+            envelope.mail_from = self.rtoe(envelope.rcpt_tos[0])
             envelope.rcpt_tos = ["{}@{}".format(self._user, self._host)]
         else:
-            envelope.mail_from = envelope.rcpt_tos[0]
+            envelope.mail_from = self.rtoe(envelope.rcpt_tos[0])
             if subjectenc:
                 envelope.rcpt_tos = re.findall(ADDRESSWRAP.decode(), ds1.decode())
                 ds1 = re.sub(ADDRESSWRAP, b'', ds1)
@@ -232,7 +250,6 @@ class MailProxyHandler:
         lineenvelopefrom = linefrom.replace(FROM, ENVELOPEFROM)
         lineto = TO + envelope.rcpt_tos[0].encode() + CRLF
         
-        # headers.append(self.dkim().encode() + CRLF)
         headers.append(self.messageid().encode() + CRLF)
         headers.append(self.received(envelope.rcpt_tos[0], CRLF).encode() + CRLF)
         headers.append(linexmailer)
@@ -245,7 +262,6 @@ class MailProxyHandler:
         
         # header plus content
         tags = (CTE, CT)
-        sts = (ST1, ST2)
         cont = False
         other = False
         for line in lines:
@@ -273,6 +289,11 @@ class MailProxyHandler:
                     
         # envelope content
         self._content = EMPTYBYTES.join(headers)
+        if dkim_selector:
+            if (not self._inbound) or self._use_dkim:
+                dkim = self.dkim(self._content)
+                self._content = dkim + self._content
+            
         return True
         
         
@@ -289,30 +310,55 @@ if __name__ == '__main__':
     config = configparser.ConfigParser()
     config.read(config_path)
     
-    __SERVER_MODE__ = config.getboolean('general', 'servermode')
+    __SERVER_MODE__ = config.getboolean('general', 'servermode', fallback = False)
     domain = config.get('server', 'domain')
+    if not domain:
+        log.warning('please set domain in config file')
+        sys.exit()
     if __SERVER_MODE__:
+        dkim_selector = config.get('server', 'dkim_selector')
+        if dkim_selector:
+            dkim_keyfile = config.get('server', 'dkim_keyfile')
+            if not dkim_keyfile:
+                log.warning('please set dkim_keyfile in config file')
+                sys.exit()
+            with open(dkim_keyfile, 'r') as f:
+                dkim_key = f.read()
+        else:
+            dkim_key = None
+            log.info('no dkim-signature')
         relay_ip = config.get('server', 'relay_ip')
         if not relay_ip:
             relay_ipfile = config.get('server', 'relay_ipfile')
             if not relay_ipfile:
                 log.warning('please set relay_ip or relay_ipfile in config file')
                 sys.exit()
-            f = open(relay_ipfile, 'r')
-            relay_ip = f.readline()
-            f.close()
+            with open(relay_ipfile, 'r') as f:
+                relay_ip = f.read()
     relay_bind = config.get('relay', 'bind', fallback = '0.0.0.0')
     relay_port = config.getint('relay', 'port', fallback = 2525)
     bind = config.get('server', 'bind', fallback = '0.0.0.0')
     port = config.getint('server', 'port', fallback = 25)
     
+    host = config.get('remote', 'host')
+    if not host:
+        log.warning('please set host in config file')
+        sys.exit()
+    user = config.get('remote', 'user')
+    if not user:
+        log.warning('please set user in config file')
+        sys.exit()
+
     controller = Controller(
         MailProxyHandler(
             domain = domain,
-            host = config.get('remote', 'host'),
-            user = config.get('remote', 'user'),
+            host = host,
+            user = user,
+            use_dkim = config.getboolean('remote', 'use_dkim', fallback = True),
             port = config.getint('remote', 'port', fallback = 25),
-            relay_ip = relay_ip,
+            dkim_selector = dkim_selector if __SERVER_MODE__ else None,
+            dkim_key = dkim_key if __SERVER_MODE__ else None,
+            relay_ip = relay_ip if __SERVER_MODE__ else None,
             relay_port = relay_port,
         ),
         hostname = bind if __SERVER_MODE__ else relay_bind,
